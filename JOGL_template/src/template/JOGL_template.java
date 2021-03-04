691e48f8ce45525a846ef78c11ef25fdf0374185
karbovskiydmitriy <karbovskiydmitriy99@gmail.com>
2021-03-04 15:51:04 +0300

    Deprecated meshes, switched to VBO-s

--- JOGL_template/src/template/JOGL_template.java
+++ JOGL_template/src/template/JOGL_template.java
@@ -5,6 +5,8 @@ import java.awt.event.WindowAdapter;
 import java.awt.event.WindowEvent;
 import java.io.File;
 import java.io.IOException;
+import java.nio.FloatBuffer;
+import java.nio.IntBuffer;
 import java.nio.file.Files;
 
 import javax.swing.JFrame;
@@ -18,8 +20,10 @@ import com.jogamp.opengl.awt.GLCanvas;
 import com.jogamp.opengl.glu.gl2.GLUgl2;
 import com.jogamp.opengl.util.Animator;
 
-import types.CompressedMesh;
-import types.Mesh;
+import static com.jogamp.opengl.GL2.*;
+
+import static template.Cube.cubeIndices;
+import static template.Cube.cubeVertices;
 
 public class JOGL_template implements GLEventListener {
 
@@ -35,6 +39,8 @@ public class JOGL_template implements GLEventListener {
 	long currentTime;
 	float angle;
 
+	int verticesVBO;
+	int indicesVBO;
 	int vertexShader;
 	int fragmentShader;
 	int renderProgram;
@@ -43,7 +49,6 @@ public class JOGL_template implements GLEventListener {
 	int screenSizeLocation1;
 	int depthBufferLocation1;
 
-	Mesh mesh;
 	Dimension dimension;
 
 	static Animator animator;
@@ -68,8 +73,21 @@ public class JOGL_template implements GLEventListener {
 		startTime = System.currentTimeMillis();
 		lastTime = startTime;
 
-		mesh = generateMesh(Cube.cubeMesh);
 		initShaders(gl2);
+
+		int[] buffer = new int[1];
+
+		gl2.glGenBuffers(1, buffer, 0);
+		verticesVBO = buffer[0];
+		gl2.glBindBuffer(GL_ARRAY_BUFFER, verticesVBO);
+		gl2.glBufferData(GL_ARRAY_BUFFER, cubeVertices.length * 4, FloatBuffer.wrap(cubeVertices),
+				GL_STATIC_DRAW);
+
+		gl2.glGenBuffers(1, buffer, 0);
+		indicesVBO = buffer[0];
+		gl2.glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indicesVBO);
+		gl2.glBufferData(GL_ELEMENT_ARRAY_BUFFER, cubeIndices.length * 4, IntBuffer.wrap(cubeIndices),
+				GL_STATIC_DRAW);
 	}
 
 	@Override
@@ -80,12 +98,12 @@ public class JOGL_template implements GLEventListener {
 		dimension = new Dimension(width - x, height - y);
 
 		gl2.glViewport(x, y, width, height);
-		gl2.glMatrixMode(GL2.GL_PROJECTION);
+		gl2.glMatrixMode(GL_PROJECTION);
 		gl2.glLoadIdentity();
 		glu2.gluPerspective(fovY, (float) dimension.height / dimension.height, zNear, zFar);
-		gl2.glEnable(GL2.GL_DEPTH_TEST);
-		gl2.glShadeModel(GL2.GL_SMOOTH);
-		gl2.glHint(GL2.GL_PERSPECTIVE_CORRECTION_HINT, GL2.GL_NICEST);
+		gl2.glEnable(GL_DEPTH_TEST);
+		gl2.glShadeModel(GL_SMOOTH);
+		gl2.glHint(GL_PERSPECTIVE_CORRECTION_HINT, GL_NICEST);
 	}
 
 	public static void main(String[] args) {
@@ -113,23 +131,6 @@ public class JOGL_template implements GLEventListener {
 		frame.setVisible(true);
 	}
 
-	private static Mesh generateMesh(CompressedMesh sourceMesh) {
-		int verticesCount = sourceMesh.trianglesCount * 3 * 3;
-		float[] vertices = new float[verticesCount];
-
-		for (int i = 0, index = 0; i < sourceMesh.trianglesCount; i++) {
-			for (int j = 0; j < 3; j++) {
-				int vertexIndex = sourceMesh.triangles[i][j];
-				for (int k = 0; k < 3; k++) {
-					vertices[index] = sourceMesh.vertices[vertexIndex][k];
-					index++;
-				}
-			}
-		}
-
-		return new Mesh(sourceMesh.trianglesCount, vertices);
-	}
-
 	private void initShaders(GL2 gl2) {
 		gl2.glUseProgram(0);
 
@@ -141,14 +142,14 @@ public class JOGL_template implements GLEventListener {
 			gl2.glDeleteProgram(renderProgram);
 		}
 
-		vertexShader = loadShader(gl2, ".\\shaders\\vertex.glsl", GL2.GL_VERTEX_SHADER);
+		vertexShader = loadShader(gl2, ".\\shaders\\vertex.glsl", GL_VERTEX_SHADER);
 		if (vertexShader == 0) {
 			renderProgram = 0;
 
 			return;
 		}
 
-		fragmentShader = loadShader(gl2, ".\\shaders\\fragment.glsl", GL2.GL_FRAGMENT_SHADER);
+		fragmentShader = loadShader(gl2, ".\\shaders\\fragment.glsl", GL_FRAGMENT_SHADER);
 		if (fragmentShader == 0) {
 			renderProgram = 0;
 
@@ -179,7 +180,7 @@ public class JOGL_template implements GLEventListener {
 		gl2.glLinkProgram(program);
 
 		int[] linked = new int[1];
-		gl2.glGetProgramiv(program, GL2.GL_LINK_STATUS, linked, 0);
+		gl2.glGetProgramiv(program, GL_LINK_STATUS, linked, 0);
 
 		if (linked[0] != 0) {
 			return program;
@@ -202,11 +203,15 @@ public class JOGL_template implements GLEventListener {
 		try {
 			String shaderText = new String(Files.readAllBytes(new File(fileName).toPath()));
 			int shader = gl2.glCreateShader(shaderType);
-			gl2.glShaderSource(shader, 1, new String[] { shaderText }, new int[] { shaderText.length() }, 0);
+			gl2.glShaderSource(shader, 1, new String[] {
+					shaderText
+			}, new int[] {
+					shaderText.length()
+			}, 0);
 			gl2.glCompileShader(shader);
 
 			int[] compiled = new int[1];
-			gl2.glGetShaderiv(shader, GL2.GL_COMPILE_STATUS, compiled, 0);
+			gl2.glGetShaderiv(shader, GL_COMPILE_STATUS, compiled, 0);
 
 			if (compiled[0] != 0) {
 				return shader;
@@ -228,9 +233,9 @@ public class JOGL_template implements GLEventListener {
 		}
 
 		gl2.glClearColor(1f, 1f, 1f, 1f);
-		gl2.glClear(GL2.GL_COLOR_BUFFER_BIT | GL2.GL_DEPTH_BUFFER_BIT);
+		gl2.glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-		gl2.glMatrixMode(GL2.GL_MODELVIEW);
+		gl2.glMatrixMode(GL_MODELVIEW);
 		gl2.glLoadIdentity();
 		glu2.gluLookAt(0f, SIZE * 1.8f, SIZE * 3.6f, 0f, 0f, 0f, 0f, 1f, 0f);
 		gl2.glRotatef(angle, 0f, 1f, 0f);
@@ -241,12 +246,12 @@ public class JOGL_template implements GLEventListener {
 			gl2.glUniform2f(screenSizeLocation1, (float) dimension.width, (float) dimension.height);
 		}
 
-		gl2.glEnableClientState(GL2.GL_VERTEX_ARRAY);
-
-		gl2.glVertexPointer(3, GL2.GL_FLOAT, 0, mesh.vertices);
-		gl2.glDrawArrays(GL2.GL_TRIANGLES, 0, mesh.verticesCount);
-
-		gl2.glDisableClientState(GL2.GL_VERTEX_ARRAY);
+		gl2.glBindBuffer(GL_ARRAY_BUFFER, verticesVBO);
+		gl2.glVertexPointer(3, GL_FLOAT, 0, 0);
+		gl2.glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, indicesVBO);
+		gl2.glEnableClientState(GL_VERTEX_ARRAY);
+		gl2.glDrawElements(GL_TRIANGLES, 24, GL_UNSIGNED_INT, 0);
+		gl2.glDisableClientState(GL_VERTEX_ARRAY);
 
 		gl2.glUseProgram(0);
 	}
